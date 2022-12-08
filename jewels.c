ba0ca2e70967947e1882f407697d88d9521d5601
Pedro Amaral <amaral1@ufpr.br>
2022-12-08 11:45:45 -0300

    Create jewels.c

---
+++ jewels.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+    Implementação do jogo 'Jewels' em C
+    Desenvolvido por Pedro Amaral Chapelin
+    Finalizado em - XX/XX/XXXX
+*/
+
+int main ()
+{
+  
+  
+    return 0;
+}
